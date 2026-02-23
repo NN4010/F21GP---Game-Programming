5e4e3cbeee6a1d0cdf96358fbd3031fee4176dfc
NN4010 <nn4010@hw.ac.uk>
2026-02-23 22:57:52 +0400

    Player Movement Script Added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+       controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+
     }
 }
